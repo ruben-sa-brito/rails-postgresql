dc1310e84edcf12ca5fec3c5918455abf3832f2d
Ruben Sa <rubenjms1@gmail.com>
2024-06-09 17:22:06 -0300

    incluindo primeiro teste unitario

--- app/models/administrador.rb
+++ app/models/administrador.rb
@@ -1,2 +1,5 @@
 class Administrador < ApplicationRecord
+    validates :nome, presence: true, length: { maximum: 150}
+    validates :email, presence: true, length: { maximum: 255}
+    validates :senha, presence: true, length: { maximum: 255}
 end
--- test/models/administrador_test.rb
+++ test/models/administrador_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class AdministradorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+    def setup
+        @administrador = Administrador.new(nome: "Luiz", email: "lu@gmail.com", senha: "12345")      
+    end
+    
+    test "deve ter o objeto administrador valido" do
+        #Arrange
+        @administrador = Administrador.new(nome: "Luiz", email: "lu@gmail.com", senha: "12345")
+        #Act
+        valido = @administrador.valid?
+        #Assert
+        assert valido
+    end    
+end 
+
